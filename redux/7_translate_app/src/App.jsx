8517219cdc59c7098431b4eba4ec386063c3a275
furkanevin <furkanevin00@gmail.com>
2024-04-20 21:23:48 +0300

    translate app

--- redux/7_translate_app/src/App.jsx
+++ redux/7_translate_app/src/App.jsx
@@ -2,11 +2,13 @@ import { useEffect, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getLanguages, translateText } from './redux/actions';
 import Select from 'react-select';
+import { setAnswer } from './redux/slices/translateSlice';
 
 const App = () => {
   const { isLoading, error, languages } = useSelector(
     (store) => store.languageReducer
   );
+  const translateState = useSelector((store) => store.translateReducer);
 
   const [sourceLang, setSourceLang] = useState({
     label: 'Turkish',
@@ -42,6 +44,18 @@ const App = () => {
     dispatch(translateText({ sourceLang, targetLang, text }));
   };
 
+  const handleSwap = () => {
+    // select alanlarındaki veileri yer değiştir
+    setSourceLang(targetLang);
+    setTargetLang(sourceLang);
+
+    // cevap alanındaki veriyi çevir alanına aktar
+    setText(translateState.answer);
+
+    // çeviri alanındaki veriyi cevap alanına aktar
+    dispatch(setAnswer(text));
+  };
+
   return (
     <div className="bg-zinc-900 h-screen text-white grid place-items-center">
       <div className="w-[80vw] max-w-[1100px] flex flex-col justify-center">
@@ -50,17 +64,24 @@ const App = () => {
         {/* üst kısım */}
         <div className="flex gap-2 text-black">
           <Select
+            isDisabled={isLoading}
+            isLoading={isLoading}
             value={sourceLang}
             onChange={(e) => setSourceLang(e)}
             className="flex-1"
             options={formatted}
           />
 
-          <button className="rounded py-2 px-6 bg-zinc-700 text-white transition hover:ring-2 hover:bg-zinc-800">
+          <button
+            onClick={handleSwap}
+            className="rounded py-2 px-6 bg-zinc-700 text-white transition hover:ring-2 hover:bg-zinc-800"
+          >
             Değiş
           </button>
 
           <Select
+            isDisabled={isLoading}
+            isLoading={isLoading}
             value={targetLang}
             onChange={(e) => setTargetLang(e)}
             className="flex-1"
@@ -72,16 +93,27 @@ const App = () => {
         <div className="flex mt-5 gap-3 md:gap-[105px] max-md:flex-col">
           <div className="flex-1">
             <textarea
+              value={text}
               onChange={(e) => setText(e.target.value)}
               className="w-full min-h-[300px] max-h-[500px] p-[10px] text-[20px] rounded text-black"
             ></textarea>
           </div>
 
-          <div className="flex-1">
+          <div className="flex-1 relative">
             <textarea
+              value={translateState.answer}
               disabled
-              className="w-full min-h-[300px] max-h-[500px] p-[10px] text-[20px] rounded text-black"
+              className="w-full min-h-[300px] max-h-[500px] p-[10px] text-[20px] rounded text-gray-300"
             ></textarea>
+
+            {translateState.isLoading && (
+              <div className="loader absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+              </div>
+            )}
           </div>
         </div>
 
--- redux/7_translate_app/src/redux/store.js
+++ redux/7_translate_app/src/redux/store.js
@@ -1,6 +1,7 @@
 import languageReducer from './slices/languageSlice';
+import translateReducer from './slices/translateSlice';
 import { configureStore } from '@reduxjs/toolkit';
 
 export default configureStore({
-  reducer: { languageReducer },
+  reducer: { languageReducer, translateReducer },
 });
